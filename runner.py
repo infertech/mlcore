19048870e7326853ef9ee98c665140eb485d55dd
Andrey Volkov <andrew.wolf.alex@gmail.com>
2019-04-25 21:11:42 +0300

    Remove temporary debugging code

--- runner.py
+++ runner.py
@@ -11,11 +11,3 @@ def run(nodes, data):
     for node in nodes:
         data = node.process(data)
     return data
-
-
-if __name__ == '__main__':
-    import sys
-    number = int(sys.argv[1])
-    nodes = [BaseNode() for _ in range(number)]
-    result = run(nodes, sys.argv[2])
-    print(result)
