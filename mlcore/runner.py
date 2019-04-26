04a1b0583e29373a4b5341fef4a0f6637f9c5ed2
Andrey Volkov <andrew.wolf.alex@gmail.com>
2019-04-26 11:52:21 +0300

    Add logging to runner

--- mlcore/runner.py
+++ mlcore/runner.py
@@ -1,13 +1,28 @@
+from datetime import datetime
+
 from .exceptions import NotNodeException
 from .nodes import BaseNode
 
 
 def run(nodes, data):
     # Verify argument types
-    for node in nodes:
+    for name, node in nodes:
         if not isinstance(node, BaseNode):
             raise NotNodeException(klass=node.__class__)
+
+    global_log = []
     # Start data processing
-    for node in nodes:
+    time_before = datetime.now()
+    for name, node in nodes:
+        global_log.append((time_before, f"Started processing {name}"))
+        # Process node and save its log
         data = node.process(data)
-    return data
+        global_log += node.log_list
+        # Log execution time
+        time_after = datetime.now()
+        delta = time_after - time_before
+        global_log.append(
+            (time_after, f"Finished processing {name} in {delta}"))
+        time_before = time_after
+
+    return data, global_log
