418dbeb3b75a551995e9a1460a475dd1413f3a9a
Andrey Volkov <andrew.wolf.alex@gmail.com>
2019-04-26 11:13:03 +0300

    Add log method to nodes

--- mlcore/nodes/base.py
+++ mlcore/nodes/base.py
@@ -1,5 +1,18 @@
+from datetime import datetime
+
+
 class BaseNode:
+
+    def __init__(self, params):
+        self.params = params
+        self.log_list = []
 
     def process(self, data):
         raise NotImplementedError(
-            "Nodes should redefine their 'proceeess' method")
+            "Nodes should redefine their 'process' method")
+
+    def log(self, data):
+        """Log string representation of the data with timestamp"""
+        string = str(data)
+        timestamp = datetime.now()
+        self.log_list.append((timestamp, string))
